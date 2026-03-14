7e967ef5e4f8a03c88109726d7d457766df0ab17
ShihyunKao <kkao001@gold.ac.uk>
2026-03-14 15:03:18 +0000

    update

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-// 告诉服务器，网页文件都在 public 文件夹里
+// Tell the server that all the web files are in the public folder.
 app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log('有新连接:', socket.id);
 
-    // 监听手机端发来的 'throw' 事件
+    // Listen for the 'throw' event sent from the mobile phone end
     socket.on('throw', (data) => {
-        // 收到后，立刻广播给所有连接者（包括大屏幕）
+        // Upon receipt, immediately broadcast to all connected parties (including the large screen)
         io.emit('new_envelope', data);
     });
 });
 
-// 使用 Render 分配的端口，或者是本地的 3000
+// Use the port assigned by Render, or the local port 3000
 const port = process.env.PORT || 3000;
 http.listen(port, () => {
     console.log(`Server running on port ${port}`);
